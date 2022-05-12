c1d129f70bb427a04739a013b1545b1c745ae1ec
Maxim Kudryashov <m.kudryashov@kt-team.de>
2022-05-12 15:37:24 +0400

    Disable data widget, update styles

--- ubersicht/kud-top-bar/lib/components/space.jsx
+++ ubersicht/kud-top-bar/lib/components/space.jsx
@@ -2,7 +2,7 @@ const Space = ({ space }) => {
   return (
     <div
       className={`transition duration-150 ease-in-out w-[1.5em] h-[1.5em] flex items-center
-      justify-center mr-1 rounded-sm relative ${space["has-focus"] ? "bg-blue-600" : "bg-blue-900"}`}
+      justify-center mr-1 rounded-sm relative ${space["has-focus"] ? "bg-slate-900" : "bg-slate-700"}`}
     >
       {space.index}
       <div className="absolute -top-[12px] right-0">
--- ubersicht/kud-top-bar/spaces.jsx
+++ ubersicht/kud-top-bar/spaces.jsx
@@ -2,10 +2,13 @@ import Spaces from "./lib/widgets/spaces-list.jsx";
 
 import { getYabaiData } from "./lib/utils/yabai";
 
+import { resetStyles } from "./lib/utils/reset";
+
 const command = "bash ./kud-top-bar/init.sh";
 
 const refreshFrequency = false;
 
+resetStyles();
 const render = ({ output }) => {
   const { spaces, displays, windows } = getYabaiData(output);
 
@@ -15,13 +18,13 @@ const render = ({ output }) => {
     .sort((a, b) => a.pid - b.pid);
 
   return (
-    <div className="flex items-center px-5 py-1 text-sm">
+    <div className="flex items-center px-5 py-1 text-sm bg-slate-50 w-screen bg-opacity-50 backdrop-blur-2xl">
       <Spaces spaces={spaces} displays={displays} />
       {spaceWindows.map((window) => (
         <span
           key={window.id}
           className={`transition duration-150 ease-in-out mx-1 px-2 rounded-sm ${
-            window["has-focus"] && "bg-blue-600 text-white"
+            window["has-focus"] && "bg-slate-900 text-white"
           }`}
         >
           {window.app}
